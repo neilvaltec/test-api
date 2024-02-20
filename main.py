428756605e7594a9385f2c638e99a323d60716ce
Neil Chou <neil@valtec.io>
2024-02-21 02:17:11 +0800

    dev

--- main.py
+++ main.py
@@ -9,3 +9,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8080)
